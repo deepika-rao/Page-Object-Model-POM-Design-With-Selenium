b0697af71b2d6d48e1544dec34ee937f8b548e39
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-07-03 22:06:20 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -1,9 +1,9 @@
 package com.crm.qa.util;
 
 public class TestUtil {
-	
-	public static long PAGE_LOAD_TIMEOUT = 20;
-	public static long IMPLICIT_WAIT = 10;
+
+	public static long PAGE_LOAD_TIMEOUT = 60;
+	public static long IMPLICIT_WAIT = 20;
 	
 	
 	
