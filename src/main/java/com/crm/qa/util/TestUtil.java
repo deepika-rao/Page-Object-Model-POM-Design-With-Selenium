46a44df3354001cfd1f757f08c4f43aa29216eea
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-07-04 08:28:01 +0530

    Update TestUtil.java

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -1,9 +1,15 @@
 package com.crm.qa.util;
 
-public class TestUtil {
+import com.crm.qa.base.TestBase;
 
+public class TestUtil extends TestBase{
+	
 	public static long PAGE_LOAD_TIMEOUT = 60;
 	public static long IMPLICIT_WAIT = 20;
+	
+	public void switchToFrame(){
+		driver.switchTo().frame("mainpanel");
+	}
 	
 	
 	
