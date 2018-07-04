c9990319ec9e38153cb3f31141485fcdc32b552f
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-07-04 08:31:02 +0530

    Update HomePageTest.java

--- src/test/java/com/crm/qa/testcases/HomePageTest.java
+++ src/test/java/com/crm/qa/testcases/HomePageTest.java
@@ -1,22 +1,52 @@
 package com.crm.qa.testcases;
 
+import java.util.concurrent.TimeUnit;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.HomePage;
 import com.crm.qa.pages.LoginPage;
+import com.crm.qa.util.TestUtil;
 
 public class HomePageTest extends TestBase{
 	LoginPage loginPage;
+	HomePage homePage;
+	TestUtil testUtil;
 	
 	public HomePageTest(){
 		super();//using this it will not throw null pointer exception
 	}
 	
+	// test cases should be independent, so always use login and logout for each test case
+	
 	@BeforeMethod
 	public void setUp(){
 		initialization();
+		testUtil = new TestUtil();
 		loginPage = new LoginPage();
-		loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
 	}
-
+	
+	@Test(priority=1)
+	public void verifyHomePageTitleTest(){		
+		String homePageTitle = homePage.verifyHomePageTitle();
+		Assert.assertEquals(homePageTitle, "CRMPRO","Home Page Title Not Matched");
+	}
+	
+	@Test(priority=2)
+	public void verifyUserNameTest(){
+		testUtil.switchToFrame();
+		Assert.assertTrue(homePage.verifyCorrectUserName());
+	}
+	
+	
+	@AfterMethod(enabled=true)
+	public void tearDown(){
+		driver.quit();
+	}
+	
 }
