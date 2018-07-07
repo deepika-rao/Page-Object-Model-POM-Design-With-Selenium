ce8c2d5a5538bb9edc924ad0d318de8c9cb28c93
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-07-07 20:03:41 +0530

    updated util class

--- src/main/java/com/crm/qa/util/TestUtil.java
+++ src/main/java/com/crm/qa/util/TestUtil.java
@@ -7,14 +7,50 @@ public class TestUtil extends TestBase{
 	public static long PAGE_LOAD_TIMEOUT = 60;
 	public static long IMPLICIT_WAIT = 20;
 	
+	public static String TESTDATA_SHEET_PATH = "C:\\Users\\Home\\workspace\\FreeCRMTest\\src\\main\\java\\"
+			+ "com\\crm\\qa\\testdata\\FreeCrmTestData.xlsx";
+	
+	static Workbook book;
+	static Sheet sheet;
+	
 	public void switchToFrame(){
 		driver.switchTo().frame("mainpanel");
 	}
 	
+	public static Object[][] getTestData(String sheetName) { // very imp method for data driven framework
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream(TESTDATA_SHEET_PATH);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+			book = WorkbookFactory.create(file);
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		sheet = book.getSheet(sheetName);
+		Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()]; // return type of data is 2 dimensional object array, eg: 2nd row and 1st column
+		// System.out.println(sheet.getLastRowNum() + "--------" +
+		// sheet.getRow(0).getLastCellNum());
+		for (int i = 0; i < sheet.getLastRowNum(); i++) { // 2 for loops for row and column
+			for (int k = 0; k < sheet.getRow(0).getLastCellNum(); k++) {
+				data[i][k] = sheet.getRow(i + 1).getCell(k).toString();
+				// System.out.println(data[i][k]);
+			}
+		}
+		return data;
+	}
+	
+	public static void takeScreenshotAtEndOfTest() throws IOException {
+		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+		String currentDir = System.getProperty("user.dir");		
+		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png")); // + means append with current timemillis
+		
+		}
 	
 	
-	
-	
-	
-
 }
+	
