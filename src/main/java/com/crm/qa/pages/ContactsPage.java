eb895ab02d806f5e854981ada9426fbdf22375c6
deepika-rao <35284111+deepika-rao@users.noreply.github.com>
2018-07-07 19:58:33 +0530

    updated this page

--- src/main/java/com/crm/qa/pages/ContactsPage.java
+++ src/main/java/com/crm/qa/pages/ContactsPage.java
@@ -3,5 +3,47 @@ package com.crm.qa.pages;
 import com.crm.qa.base.TestBase;
 
 public class ContactsPage extends TestBase{
+  
+@FindBy(xpath="//td[contains(text(),'Contacts')]")
+	WebElement contactsLabel;
+	
+	@FindBy(id="first_name")
+	WebElement firstName;
+	
+	@FindBy(id="surname")
+	WebElement lastName;
+	
+	@FindBy(name="client_lookup")
+	WebElement company;
+	
+	@FindBy(xpath="//input[@type='submit' and @value='Save']")
+	WebElement saveBtn;
+	
+	public ContactsPage(){
+		PageFactory.initElements(driver, this);
+	}
+	
+	public boolean verifyContactsLabel(){
+		return contactsLabel.isDisplayed();
+	}
+	
+	public void selectContactsByName(String name){
+		driver.findElement(By.xpath("//a[text()='"+name+"']//parent::td[@class='datalistrow']"
+				+ "//preceding-sibling::td[@class='datalistrow']//input[@name='contact_id']")).click();		
+	}
+	
+	// use when Data driven is not added to project
+	public void createNewContact(String title, String ftName, String ltName, String comp){
+		Select select = new Select(driver.findElement(By.name("title")));
+		select.selectByVisibleText(title);
+		
+		firstName.sendKeys(ftName);
+		lastName.sendKeys(ltName);
+		company.sendKeys(comp);
+		saveBtn.click();
+		
+	}
+	
+	
 
 }
